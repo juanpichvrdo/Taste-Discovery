2cf35b57b57dcf53a07b6ad1b3f1b66c8a8cd313
Juan Daniel Pichardo <jpichardopena@gmail.com>
2018-10-01 16:54:53 -0400

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h3>Taste Like</h3>
+      <a href="https://github.com/juanpichvrdo/Taste-Like" target="_blank">
+        Github
+      </a>
+    </header>
+  );
+};
+
+export default Header;
